aba79d616227e095b975c13b1ee4ef0217e0a5fa
Amin <amin83ghasemi@gmail.com>
2024-03-01 18:01:37 +0330

    script to compile/run test files with acc and gcc

---
+++ test.sh
@@ -0,0 +1,23 @@
+#/usr/bin/bash
+# run file in test folder using both GCC and ACC
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    exit -1
+fi
+
+input="./test/${1}.c"
+
+if [ ! -f "${input}" ]; then
+    echo "File not found: ${input}"
+    exit -1
+fi
+
+echo "GCC ----------------------"
+gcc.exe $input -o test/${1}_gcc.exe && ./test/${1}_gcc.exe; echo $?
+echo "--------------------------"
+echo
+echo "ACC ----------------------"
+cargo.exe run -- build -k --ast $input && ./test/${1}.exe; echo $?
+echo "--------------------------"
